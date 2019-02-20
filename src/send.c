7335ec9f493c3fb09e8ec7429bfde14b2434aa85
kwang40 <kaishenw1995@gmail.com>
2019-02-20 13:30:34 -0600

    change output way to solve concurrency issue

--- src/send.c
+++ src/send.c
@@ -292,7 +292,7 @@ int send_run(sock_t st, shard_t *s)
     memset(ipStr, 0, 30);
     sprintf(ipStr, "#%d.%d.%d.%d\n", n1, n2, n3, n4);
     lock_file(stdout);
-    printf("%s", ipStr);
+    zconf.probe_module->print_packet(stdout, ipStr);
     unlock_file(stdout);
 		int32_t raw_ip_addr = (n1 & 0xFF) << 24 | (n2 & 0xFF) << 16 | (n3 & 0xFF) << 8 | (n4 & 0xFF);
 		int32_t ip_addr = ntohl(raw_ip_addr);
